5e99acb65ec997577071d37a9c262c7aeee1b78a
seokwon <tjrtkd95@gmail.com>
2024-10-02 20:34:04 +0900

    feat: 챔피언 상세데이터 렌더링

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,24 +1,36 @@
-import { getChampionsData } from "@/app/api/utils/serverApi";
+import { getChampionDetail } from "@/app/api/utils/serverApi";
+import { ChampionDetail } from "@/types/champion";
 import React from "react";
 
-const page = async ({ params }: { params: { id: string } }) => {
-  const data = await getChampionsData();
+const detailPage = async ({ params }: { params: { id: string } }) => {
+  // const championDetail = await getChampionDetail(params.id)
+  const id = params.id;
 
-  const champion = data.find((champ) => champ.id === params.id);
+  const data = await getChampionDetail(id);
+  const champion = data[id];
+  // console.log("챔피언 디테일", champion);
 
   return (
-    <div
-      className="h-screen bg-black/50"
-      style={{
-        backgroundImage: `url(https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion?.id}_0.jpg)`,
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="h-screen bg-gradient-to-r from-black to-black/50">
-        챔피언 상세페이지 {params.id}
+    <>
+      <div
+        className="h-screen max-w-[1920px] w-[100%]"
+        style={{
+          backgroundImage: `url(https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_0.jpg)`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="h-screen bg-gradient-to-r from-black to-black/50 flex flex-col justify-center">
+          <div className=" mx-[30px] flex flex-col justify-around h-[70%]">
+            <div className="flex flex-col max-w-[700px] gap-5">
+              <div className="text-2xl font-bold text-titlecolor">{champion.title}</div>
+              <div className="text-5xl font-black text-white">{champion.name}</div>
+              <div className="text-lg text-white">{champion.lore}</div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default page;
+export default detailPage;
