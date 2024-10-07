a89dc40405421e0b35386bd3865b70d02e95e1db
seokwon <tjrtkd95@gmail.com>
2024-10-07 20:20:38 +0900

    style: 레이아웃 스타일 변경 및 홈페이지 디자인 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <nav className="flex flex-row gap-20 justify-around bg-slate-700 p-5 fixed w-[100%] shadow shadow-black ">
+        <nav className="flex justify-around bg-slate-700 p-5 sticky top-0 w-[100%]  max-w-[1920px] m-auto shadow shadow-black ">
           <Link className="link-style" href={"/"}>
             홈
           </Link>
@@ -44,7 +44,24 @@ export default function RootLayout({
             로테이션
           </Link>
         </nav>
-        <div className="pt-[64px] flex flex-col justify-center">{children}</div>
+        <div className="pt-[64px] flex flex-col justify-center ">{children}</div>
+        <footer className="p-5 w-[100%] max-w-[1920px] m-auto bg-slate-800 text-xs text-slate-400 ">
+          <div>
+            <p>스파르타 내일배움 캠프 | 주특기 플러스주차 개인과제</p>
+          </div>
+          <div className="flex gap-1">
+            <p>개발자: 이석원 |</p>
+            <div>
+              Git주소:{" "}
+              <Link href={`https://github.com/seokwon27`}>https://github.com/seokwon27</Link> |
+            </div>
+            <div>
+              Blog주소:{" "}
+              <Link href={`https://record165.tistory.com/`}>https://record165.tistory.com/</Link>
+            </div>
+          </div>
+          <div>개발기간: 2024.09.27 ~ 2024.10.07</div>
+        </footer>
       </body>
     </html>
   );
