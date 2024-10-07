f32a7696e2c2b41451ab6bdfcdc1a54d88274edf
seokwon <tjrtkd95@gmail.com>
2024-10-07 20:21:25 +0900

    feat: 홈페이지 소개글 추가 및 페이지 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
-export default function Home() {
-  const versionData = async (): Promise<string> => {
-    const res = await fetch("https://ddragon.leagueoflegends.com/api/versions.json").then((data) =>
-      data.json()
-    );
-    return res[0];
-  };
+import Image from "next/image";
+import Link from "next/link";
 
+export default function Home() {
   return (
-    <div>
-      <div>홈페이지</div>
-      <div>SSG 렌더링</div>
-      <div>주요 링크와 간단한 소개를 제공합니다.</div>
-      <div>사용된 api 버전은 {versionData()} 입니다.</div>
+    <div className="p-5 w-[100%] max-w-[1920px] m-auto flex flex-col items-center text-center">
+      <div className="text-4xl text-slate-700">리그 오브 레전드 백과사전</div>
+      <div className="m-5">Riot Games API를 사용하여 챔피언과 아이템 정보를 제공합니다. </div>
+      <div className="flex flex-col gap-6">
+        <Link href={"/champions"}>
+          <div>
+            <Image alt="champions_link" src={`/poro2.jpg`} width={600} height={400} />
+            <p className="text-titlecolor font-bold mt-2">챔피언 목록</p>
+          </div>
+        </Link>
+        <Link href={"/rotation"}>
+          <div>
+            <Image alt="champions_link" src={`/poro1.jpg`} width={600} height={400} />
+            <p className="text-titlecolor font-bold mt-2">금주 로테이션 목록</p>
+          </div>
+        </Link>
+        <Link href={"/items"}>
+          <div>
+            <Image alt="champions_link" src={`/poro3.jpg`} width={600} height={400} />
+            <p className="text-titlecolor font-bold mt-2">아이템 목록</p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
